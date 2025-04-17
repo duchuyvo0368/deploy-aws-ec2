b6ad9000afbbd6e11d3e3c8e6e2561adf300a9b3
duchuyvo <voduchuy016688@gmail.com>
2025-04-17 14:40:01 +0700

    fix api

--- eCommerce_Reactjs/src/axios.js
+++ eCommerce_Reactjs/src/axios.js
@@ -3,7 +3,7 @@ import _ from "lodash";
 require("dotenv").config();
 
 const instance = axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_URL || 'https://13.250.11.133',
+    baseURL: process.env.REACT_APP_BACKEND_URL || 'https://13.229.202.21/',
 
     //  withCredentials: true
 });
