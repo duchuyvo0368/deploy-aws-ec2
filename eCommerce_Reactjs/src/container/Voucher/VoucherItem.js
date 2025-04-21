8377ba06058853f7d4f93a0e63866429489f129b
duchuyvo <voduchuy016688@gmail.com>
2025-04-21 14:21:36 +0700

    fix en

--- eCommerce_Reactjs/src/container/Voucher/VoucherItem.js
+++ eCommerce_Reactjs/src/container/Voucher/VoucherItem.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-import logoVoucher from '../../../src/resources/img/logoVoucher.png'
+import logoVoucher from '../../../src/resources/img/logoVoucher.png';
 import CommonUtils from '../../utils/CommonUtils';
-import './VoucherItem.scss'
+import './VoucherItem.scss';
 function VoucherItem(props) {
     let handleSaveVoucher = () => {
-        props.sendDataFromVoucherItem(props.id)
-    }
+        props.sendDataFromVoucherItem(props.id);
+    };
 
     return (
         <div>
@@ -15,22 +15,26 @@ function VoucherItem(props) {
                     <img src={logoVoucher}></img>
                     <span>{props.name}</span>
                 </div>
-                <div className="border-center">
-
-                </div>
+                <div className="border-center"></div>
                 <div className="content-right">
                     <div className="box-content-right">
-                        <span className="name-voucher">Giảm {props.typeVoucher}</span>
-                        <span className="max-value-voucher">Giảm tối đa {CommonUtils.formatter.format(props.maxValue)}</span>
+                        <span className="name-voucher">{props.typeVoucher}% off</span>
+                        <span className="max-value-voucher">
+                            Maximum discount {CommonUtils.formatter.format(props.maxValue)}
+                        </span>
                         <div className="box-percent">
                             <div className="wrap-percent">
-                                <div style={{ width: `${props.widthPercent}%` }} className="percent"></div>
+                                <div
+                                    style={{ width: `${props.widthPercent}%` }}
+                                    className="percent"
+                                ></div>
                             </div>
-                            <span className="used-percent">Đã dùng {props.usedAmount}%</span>
+                            <span className="used-percent">Used {props.usedAmount}%</span>
                         </div>
-                        <button onClick={() => handleSaveVoucher()} className="btn-voucher">Lưu</button>
+                        <button onClick={() => handleSaveVoucher()} className="btn-voucher">
+                            Lưu
+                        </button>
                     </div>
-
                 </div>
             </div>
         </div>
